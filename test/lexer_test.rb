0fa5cc9829ab7146d6e2d3b6e5a27e4081d0b137
tspider0176 <1a38d3a10f5e8.16h@gmail.com>
2016-08-24 15:43:57 +0900

    First commit

---
+++ test/lexer_test.rb
@@ -0,0 +1,43 @@
+require 'test/unit'
+require_relative '../src/lexer'
+
+class LexerTest < Test::Unit::TestCase
+  def test_tokenize
+    code = <<-CODE
+if 1:
+  if 2:
+    print("...")
+    if false:
+      pass
+    print("done!")
+  2
+
+print "The End"
+CODE
+
+    tokens = [
+      [:IF, "if"], [:NUMBER, 1],
+        [:INDENT, 2],
+          [:IF, "if"], [:NUMBER, 2],
+          [:INDENT, 4],
+            [:IDENTIFIER, "print"], ["(", "("],
+                                      [:STRING, "..."],
+                                    [")", ")"],
+                                    [:NEWLINE, "\n"],
+            [:IF, "if"], [:FALSE, "false"],
+            [:INDENT, 6],
+              [:IDENTIFIER, "pass"],
+            [:DEDENT, 4], [:NEWLINE, "\n"],
+            [:IDENTIFIER, "print"], ["(", "("],
+                                      [:STRING, "done!"],
+                                    [")", ")"],
+        [:DEDENT, 2], [:NEWLINE, "\n"],
+        [:NUMBER, 2],
+      [:DEDENT, 0], [:NEWLINE, "\n"],
+      [:NEWLINE, "\n"],
+      [:IDENTIFIER, "print"], [:STRING, "The End"]
+    ]
+
+    assert_equal tokens, Lexer.new.tokenize(code)
+  end
+end
