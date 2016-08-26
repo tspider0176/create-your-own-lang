9887fb77cbe36f844c28e4e49b3098d2464753ac
tspider0176 <1a38d3a10f5e8.16h@gmail.com>
2016-08-26 18:09:28 +0900

    Modify parser_test

--- test/parser_test.rb
+++ test/parser_test.rb
@@ -4,13 +4,23 @@ require_relative '../src/parser'
 class ParserTest < Test::Unit::TestCase
   def test_parser
     code = <<-CODE
-    def method(a, b):
-      true
-    CODE
+def method(a, b):
+  b()
+
+def b():
+  false
+CODE
 
     nodes = Nodes.new([
       DefNode.new("method", ["a", "b"],
-        Nodes.new([TrueNode.new])
+        Nodes.new([
+          CallNode.new(nil, "b", [])
+        ])
+      ),
+      DefNode.new("b",[],
+        Nodes.new([
+          FalseNode.new
+        ])
       )
     ])
 
