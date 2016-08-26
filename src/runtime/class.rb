bd44dc8d653583c7e99151dfb6b7607f30c60810
tspider0176 <1a38d3a10f5e8.16h@gmail.com>
2016-08-26 23:12:58 +0900

    Add class.rb on runtime/

---
+++ src/runtime/class.rb
@@ -0,0 +1,34 @@
+class AwesomeClass < AwesomeObject
+  # Classes are objects in Awecome so they inherit from AwesomeObject
+
+  attr_accessor :runtime_methods
+
+  def initialize
+    @runtime_methods = {}
+    @runtime_class = Constants["Class"]
+  end
+
+  # Look up a method
+  def lookup(method_name)
+    method = @runtime_methods[method_name]
+    raise "Method not found: #{method_name}" if method.nil?
+
+    method
+  end
+
+  # Helper method to define a method on this class from Ruby.
+  def def(name, &block)
+    @runtime_mathods[name.to_s] = block
+  end
+
+  # Create a new instance of this class
+  def new
+    AwesomeObject.new(self)
+  end
+
+  # Create an instance of this Awesome class that holds a Ruby value.
+  # Like String, Number, or true.
+  def new_with_value(value)
+    AwesomeObject.new(self, value)
+  end
+end
